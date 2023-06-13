8b085111bf7d043d47a192d95d0ebebf36d7f33b
holyaustin <holyaustin@yahoo.com>
2023-06-13 17:02:40 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,3 @@
+
+  export const fileShareAddress = "0x4e75D8F85ED40aA3f73fB751b1Dfa07DEFe09C94"
+  
---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+   
+    testnet: {
+      url: `https://rpc.testnet.mantle.xyz`, 
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
